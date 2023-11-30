6d15f54a95edb4d463ed8dcb0238be06bf97120f
iloveass7 <borshonjsr@gmail.com>
2023-11-30 15:20:24 +0600

    Bookies Final

--- lib/AltPage.dart
+++ lib/AltPage.dart
@@ -17,24 +17,46 @@ class AltPage extends StatelessWidget {
       backgroundColor: Colors.blueGrey[400],
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
-        title: Text(name,style: TextStyle(color: Colors.amber),),
+        title: Text(
+          name,
+          style: TextStyle(color: Colors.amber),
+        ),
         centerTitle: true,
       ),
       body: Column(
         children: [
-          Image.network(url,height:200,fit: BoxFit.fill,),
-          SizedBox(height: 20,),
-          Text('Author : $author',style: TextStyle(color: Colors.amber),),
-          Text('Rating : $rating',style: TextStyle(color: Colors.amber),),
-          SizedBox(height: 30,),
-          Text('Description : $description',style: TextStyle(color: Colors.amber),),
+          Image.network(
+            url,
+            height: 200,
+            fit: BoxFit.fill,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Author : $author',
+            style: TextStyle(color: Colors.amber,fontSize: 20),
+          ),
+          Text(
+            'Rating : $rating',
+            style: TextStyle(color: Colors.amber,fontSize: 20),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Description : $description',
+            style: TextStyle(color: Colors.amber),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.pop(context);
         },
         child: Icon(Icons.arrow_back_outlined),
+        backgroundColor: Colors.blueGrey,
+
       ),
     );
   }
