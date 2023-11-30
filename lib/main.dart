0627ca5a855b63aad253384449e2f9dd07d961e9
iloveass7 <borshonjsr@gmail.com>
2023-11-30 10:35:10 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[400],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Bookies',style: TextStyle(color: Colors.amber)),
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
+
+
